58ad312f2cefb55162e84adf476626bcfb348d35
Igor <igavrilov@iconnectpos.com>
2018-02-13 21:00:59 +0200

    commands refactored to have dynamic magic

--- TradingBot.CommandPrompt/Commands.cs
+++ TradingBot.CommandPrompt/Commands.cs
@@ -5,7 +5,10 @@ namespace TradingBot.CommandPrompt
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using TradingBot.Common;
     using TradingBot.Core;
+    using TradingBot.Core.Enums;
+    using TradingBot.Data.Entities;
     using TradingBot.Services;
     using Yobit.Api;
 
@@ -27,10 +30,6 @@ namespace TradingBot.CommandPrompt
 
     public class Command
     {
-        public static List<Command> Commands;
-
-        public static Command None = new Command { Type = CommandEnum.None };
-
         public CommandEnum Type { get; set; }
 
         public List<string> Aliases { get; set; }
@@ -69,38 +68,46 @@ namespace TradingBot.CommandPrompt
             Description = description;
             AllowAnonymous = allowAnonym;
         }
+    }
 
-        static Command()
+    public class CommandsHelper
+    {
+
+        public static List<Command> List;
+
+        public static Command None = new Command { Type = CommandEnum.None };
+
+        static CommandsHelper()
         {
-            Commands = new List<Command>();
+            List = new List<Command>();
 
-            Commands.Add(None);
+            List.Add(None);
 
-            Commands.Add(
+            List.Add(
                 new Command(CommandEnum.Exit, new List<string> { "exit", "e", "finish", "esc" }, "Finish the program, amazing?")
             );
 
-            Commands.Add(
+            List.Add(
                 new Command(CommandEnum.Help, new List<string> { "help", "h", "?" }, "Show all valid commands")
             );
 
-            Commands.Add(
+            List.Add(
                 new Command(CommandEnum.RegisterUser, new List<string> { "register", "signup", "create-user" }, "Register user to allow to add accounts and get more private functionality. Parameters (* - required): username*, password*")
             );
 
-            Commands.Add(
+            List.Add(
                 new Command(CommandEnum.Login, new List<string> { "login", "signin", "let-me-enter", "authorization" }, "Sign in to add accounts and get more private functionality. Parameters (* - required): username*, password*")
             );
 
-            Commands.Add(
+            List.Add(
                 new Command(CommandEnum.Logout, new List<string> { "logout", "signoff", "signout", "lock" }, false, "Allow you to sign out, you will not have access to private functionality")
             );
 
-            Commands.Add(
+            List.Add(
                 new Command(CommandEnum.AddAccount, new List<string> { "addaccount", "add-account" }, false, "Allow you to add private Api key")
             );
 
-            Commands.Add(
+            List.Add(
                new Command(CommandEnum.GetAccounts, new List<string> { "my-accounts", "get-accounts" }, false, "Allow you to view all your accounts (for example to know which accountId you should use when run private commands)")
            );
 
@@ -108,22 +115,299 @@ namespace TradingBot.CommandPrompt
             //    new Command(CommandEnum.RemoveAccount, new List<string> { "removeaccount", "remove-account", "rem-account", "del-account", "delete-account" }, false, "Allow you to remove private Api key")
             //);
 
-            Commands.Add(
-                new Command(CommandEnum.GetActiveOrders, new List<string> { "get-orders", "orders"}, false, "Allow you to get your active orders")
+            List.Add(
+                new Command(CommandEnum.GetActiveOrders, new List<string> { "get-orders", "orders" }, false, "Allow you to get your active orders")
             );
 
-            Commands.Add(
+            List.Add(
               new Command(CommandEnum.GetPairs, new List<string> { "getpairs", "all", "info" },
               string.Format("Get all list of tickers with basic statistics. Parameters (* - required): exchangeType* ({0})",
               ExchangeInfo.GetAccountTypes))
             );
 
-            Commands.Add(
+            List.Add(
               new Command(CommandEnum.GetPairInfo, new List<string> { "getpairinfo", "tickerinfo", "get-ticker-info", "get-pair-info", "pair-info", "ticker-info" },
               string.Format("Get basic ticker info. It uses info stored from last 'getpairs'. Parameters (* - required): exchangeType* ({0}), tickerCode* ", ExchangeInfo.GetAccountTypes))
             );
         }
 
+        public User CurrentUser { get; private set; }
 
+        public CommandsHelper()
+        {
+        }
+
+        public void ExecuteCommand(CommandEnum command, params string[] list)
+        {
+            var com = command.ToString();
+            var method = typeof(CommandsHelper).GetMethod(com);
+            if (method == null)
+            {
+                Console.WriteLine("Such method is not supported");
+                return;
+            }
+            method.Invoke(this, new[] { list });
+        }
+
+        public void Help(params string[] list)
+        {
+            Console.WriteLine("\n\rAvailable commands:\n\r\n\r" +
+                string.Join("\n\r", List.Where(m => m.Type != CommandEnum.None)
+                .Select(m => m.Info)));
+        }
+
+        public void RegisterUser(params string[] list)
+        {
+            if (list.Length != 2)
+            {
+                Console.WriteLine("You must enter your username and password");
+                return;
+            }
+
+            using (var usrService = new UserService())
+            {
+                var usr = usrService.RegisterUser(list[0].Trim(), list[1].Trim());
+                if (usr == null)
+                {
+                    Console.WriteLine("Such username already registered");
+                    return;
+                }
+
+                Console.WriteLine("Registration complete");
+
+                CurrentUser = usr;
+            }
+        }
+
+        public void Login(params string[] list)
+        {
+            if (list.Length != 2)
+            {
+                Console.WriteLine("You must enter your username and password");
+                return;
+            }
+
+            using (var usrService = new UserService())
+            {
+                var usr = usrService.AuthenticateUser(list[0].Trim(), list[1].Trim());
+                if (usr == null)
+                {
+                    Console.WriteLine("Incorrect username or password");
+                    return;
+                }
+
+                Console.WriteLine("You are logged in");
+
+                CurrentUser = usr;
+            }
+        }
+
+        public void Logout(params string[] list)
+        {
+            if (CurrentUser == null)
+                Console.WriteLine("You are not authorized");
+
+            CurrentUser = null;
+        }
+
+        public void GetAccounts(params string[] list)
+        {
+            if (CurrentUser == null)
+                Console.WriteLine("You are not authorized");
+
+            using (var accService = new AccountService())
+            {
+                var result = accService.GetAccounts(CurrentUser.Id);
+
+                Console.WriteLine(string.Format("Your Account(s):\n\r {0}",
+                    string.Join("\n\r * ", result.Select(m => JsonHelper.ToJson(m)))));
+            }
+            return;
+        }
+
+        public void AddAccount(params string[] list)
+        {
+            if (CurrentUser == null)
+                Console.WriteLine("You are not authorized");
+
+            if (list.Length < 3)
+            {
+                Console.WriteLine("You must enter: any name, exchange type, api key and settings according your exchange type");
+                return;
+            }
+
+            int type;
+            string settings;
+
+            if (!int.TryParse(list[1], out type))
+            {
+                Console.WriteLine(string.Format("You must enter valid Exchange type: {0}", ExchangeInfo.GetAccountTypes));
+                return;
+            }
+            var typeEnum = (AccountType)type;
+
+            switch (typeEnum)
+            {
+                case AccountType.Yobit:
+                    if (list.Length != 4)
+                    {
+                        Console.WriteLine("For Yobit you must to enter Secret as 4th parameter");
+                        return;
+                    }
+                    settings = JsonHelper.ToJson(new YobitSettings
+                    {
+                        Secret = list[3]
+                    });
+                    break;
+                default:
+                    settings = "";
+                    break;
+            }
+
+            using (var accService = new AccountService())
+            {
+                var result = accService.CreateOrUpdate(CurrentUser.Id, list[0], typeEnum, list[2], settings);
+
+                Console.WriteLine(string.Format("Account successfully added: {0}", JsonHelper.ToJson(result)));
+            }
+            return;
+        }
+
+        public void GetActiveOrders(params string[] list)
+        {
+            if (CurrentUser == null)
+                Console.WriteLine("You are not authorized");
+
+            if (list.Length != 2 || string.IsNullOrWhiteSpace(list[1]))
+            {
+                Console.WriteLine("You must enter your account Id/Name and pair code");
+                return;
+            }
+
+            var pair = list[1].ToLowerInvariant();
+            int accId = 0;
+            string accName = "";
+
+            if (!int.TryParse(list[0], out accId))
+            {
+                accId = 0;
+                accName = list[0].ToLowerInvariant();
+            }
+
+            using (var accService = new AccountService())
+            {
+                var account = accId > 0 ? accService.GetById(accId) : accService.GetByName(CurrentUser.Id, accName);
+                if (account == null)
+                {
+                    Console.WriteLine("Can't find such account");
+                    return;
+                }
+                //todo requires refactoring
+                var exchange = ExchangeInfo.Exchanges[account.Type];
+                switch (account.Type)
+                {
+                    case AccountType.Yobit:
+                        {
+                            using (var api = exchange.Api)
+                            {
+                                var yobitApi = api as YobitApi;
+                                var result = yobitApi.GetActiveOrdersOfUser(pair, account);
+
+                                if (result == null || result.success != 1)
+                                    Console.WriteLine(string.Format("Something wrong: {0}", result.error));
+                                else
+                                {
+                                    var yobitSettings = account.YobitSettings;
+                                    if (yobitSettings.Counter == 0)
+                                        yobitSettings.Counter = 1;
+                                    else
+                                        yobitSettings.Counter++;
+
+                                    accService.UpdateSettings(account.Id, JsonHelper.ToJson(yobitSettings));
+
+                                    Console.WriteLine("Done: " + JsonHelper.ToJson(result));
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Is not supported yet.");
+                        break;
+                }
+            }
+
+            return;
+        }
+
+        public void GetPairInfo(params string[] list)
+        {
+            if (list.Length != 2)
+            {
+                Console.WriteLine("You must enter Exchange type and Ticker code");
+                return;
+            }
+            var tickerCode = list[1];
+            var param = list[0];
+            int type;
+            if (!int.TryParse(param, out type))
+            {
+                Console.WriteLine(string.Format("You must enter valid Exchange type: {0}", ExchangeInfo.GetAccountTypes));
+                return;
+            }
+
+            using (var pairService = new PairService())
+            {
+                var info = pairService.GetPair((AccountType)type, tickerCode);
+                if (info == null)
+                    Console.WriteLine("Nothing found");
+                else
+                    Console.WriteLine(string.Format("Ticker info: {0}", JsonHelper.ToJson(info)));
+            }
+        }
+
+        public void GetPairs(params string[] list)
+        {
+            if (list.Length != 1)
+            {
+                Console.WriteLine("You must enter only Exchange type");
+                return;
+            }
+
+            var param = list.FirstOrDefault();
+            int type = 0;
+            if (!int.TryParse(param, out type))
+            {
+                Console.WriteLine(string.Format("You must enter valid Exchange type: {0}", ExchangeInfo.GetAccountTypes));
+                return;
+            }
+
+            var eType = (AccountType)type;
+            if (!ExchangeInfo.Exchanges.ContainsKey(eType))
+            {
+                Console.WriteLine("It's not implemented Exchange type");
+                return;
+            }
+
+            var exchange = ExchangeInfo.Exchanges[eType];
+            using (ExchangeApi api = exchange.Api)
+            {
+                try
+                {
+                    dynamic result = null;
+                    using (var pairService = new PairService())
+                    {
+                        result = pairService.PullPairs(api);
+                    }
+                    if (result != null && result.IsSuccess)
+                        Console.WriteLine("Pairs info received, you can use /tickerInfo [tickerCode] to get appropriate info");
+                    else
+                        Console.WriteLine(string.Format("Error when pull pairs: {0}", result));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unexpected error");
+                }
+            }
+        }
     }
 }
--- TradingBot.CommandPrompt/Program.cs
+++ TradingBot.CommandPrompt/Program.cs
@@ -17,327 +17,40 @@ namespace TradingBot.CommandPrompt
 
 		}
 
-		static User CurrentUser { get; set; }
-
-
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Use help to get list of commands");
 			var input = "";
-			var command = Command.None;
-			do
-			{
-				Console.Write(string.Format("{0}> ", CurrentUser == null ? "Anonymous" : CurrentUser.Username));
-				input = Console.ReadLine().Trim();
-				if (input.Length < 1)
-					continue;
-				var parts = input.Split(' ');
 
-				command = Command.Commands.FirstOrDefault(m => m.Aliases.Contains(parts[0].Trim()));
-				if (command == null)
-					command = Command.None;
+            var commandsHelper = new CommandsHelper();
 
-				var parameters = parts.Skip(1).ToArray();
-				if (!command.AllowAnonymous && CurrentUser == null)
-					Console.WriteLine("You are not authorized");
-				else
-					switch (command.Type)
-					{
-						case CommandEnum.Help:
-                            Help(parameters);
-                            break;
-						case CommandEnum.RegisterUser:
-							CurrentUser = RegisterUser(parameters);
-							break;
-						case CommandEnum.Login:
-							CurrentUser = Login(parameters);
-							break;
-						case CommandEnum.Logout:
-							CurrentUser = Logout(parameters);
-							break;
-                        case CommandEnum.AddAccount:
-                            AddAccount(parameters);
-                            break;
-                        case CommandEnum.GetAccounts:
-                            GetAccounts(parameters);
-                            break;
-                        case CommandEnum.GetPairs:
-							GetPairs(parameters);
-							break;
-						case CommandEnum.GetPairInfo:
-							GetPairInfo(parameters);
-							break;
-                        case CommandEnum.GetActiveOrders:
-                            GetActiveOrders(parameters);
-                            break; 
+            var command = CommandsHelper.None;
+            do
+            {
+                Console.Write(string.Format("{0}> ", commandsHelper.CurrentUser == null ? "Anonymous" : commandsHelper.CurrentUser.Username));
+                input = Console.ReadLine().Trim();
+                if (input.Length < 1)
+                    continue;
+                var parts = input.Split(' ');
 
-                    }
-			}
-			while (command.Type != CommandEnum.Exit);
+                command = CommandsHelper.List.FirstOrDefault(m => m.Aliases.Contains(parts[0].Trim()));
+                if (command == null)
+                    command = CommandsHelper.None;
+
+                var parameters = parts.Skip(1).ToArray();
+                if (!command.AllowAnonymous && commandsHelper.CurrentUser == null)
+                    Console.WriteLine("You are not authorized");
+                else
+                {
+                    commandsHelper.ExecuteCommand(command.Type, parameters);
+                }
+            }
+            while (command.Type != CommandEnum.Exit);
 
 			Console.Write("You are finished. Thanks");
 			Console.ReadLine();
 		}
 
-        private static void Help(params string[] list)
-        {
-            Console.WriteLine("\n\rAvailable commands:\n\r\n\r" +
-                string.Join("\n\r", Command.Commands.Where(m => m.Type != CommandEnum.None)
-                .Select(m => m.Info)));
-        }
-
-        private static User RegisterUser(params string[] list)
-		{
-			if (list.Length != 2)
-			{
-				Console.WriteLine("You must enter your username and password");
-				return null;
-			}
-
-			using (var usrService = new UserService())
-			{
-				var usr = usrService.RegisterUser(list[0].Trim(), list[1].Trim());
-				if (usr == null)
-				{
-					Console.WriteLine("Such username already registered");
-					return usr;
-				}
-
-				Console.WriteLine("Registration complete");
-
-				return usr;
-			}
-		}
-
-		private static User Login(params string[] list)
-		{
-			if (list.Length != 2)
-			{
-				Console.WriteLine("You must enter your username and password");
-				return null;
-			}
-
-			using (var usrService = new UserService())
-			{
-				var usr = usrService.AuthenticateUser(list[0].Trim(), list[1].Trim());
-				if (usr == null)
-				{
-					Console.WriteLine("Incorrect username or password");
-					return usr;
-				}
-
-				Console.WriteLine("You are logged in");
-
-				return usr;
-			}
-		}
-
-		private static User Logout(params string[] list)
-		{
-			if (CurrentUser == null)
-				Console.WriteLine("You are not authorized");
-
-            return null;
-        }
-
-        private static void GetAccounts(params string[] list)
-        {
-            if (CurrentUser == null)
-                Console.WriteLine("You are not authorized");
-
-            using (var accService = new AccountService())
-            {
-                var result = accService.GetAccounts(CurrentUser.Id);
-
-                Console.WriteLine(string.Format("Your Account(s):\n\r {0}",
-                    string.Join("\n\r * ", result.Select(m => JsonHelper.ToJson(m)))));
-            }
-            return;
-        }
-
-        private static void AddAccount(params string[] list)
-        {
-            if (CurrentUser == null)
-                Console.WriteLine("You are not authorized");
-
-            if (list.Length < 3)
-            {
-                Console.WriteLine("You must enter: any name, exchange type, api key and settings according your exchange type");
-                return;
-            }
-
-            int type;
-            string settings;
-
-            if(!int.TryParse(list[1], out type))
-            {
-                Console.WriteLine(string.Format("You must enter valid Exchange type: {0}", ExchangeInfo.GetAccountTypes));
-                return;
-            }
-            var typeEnum = (AccountType)type;
-
-            switch(typeEnum)
-            {
-                case AccountType.Yobit:
-                    if (list.Length != 4)
-                    {
-                        Console.WriteLine("For Yobit you must to enter Secret as 4th parameter");
-                        return;
-                    }
-                    settings = JsonHelper.ToJson(new YobitSettings
-                    {
-                        Secret = list[3]
-                    });
-                    break;
-                default:
-                    settings = "";
-                    break;
-            }
-
-            using (var accService = new AccountService())
-            {            
-                var result = accService.CreateOrUpdate(CurrentUser.Id, list[0], typeEnum, list[2], settings);
-
-                Console.WriteLine(string.Format("Account successfully added: {0}", JsonHelper.ToJson(result)));
-            }
-            return;
-        }
-
-        private static void GetActiveOrders(params string[] list)
-        {
-            if (CurrentUser == null)
-                Console.WriteLine("You are not authorized");
-
-            if (list.Length != 2 || string.IsNullOrWhiteSpace(list[1]))
-            {
-                Console.WriteLine("You must enter your account Id/Name and pair code");
-                return;
-            }
-
-            var pair = list[1].ToLowerInvariant();
-            int accId = 0;
-            string accName = "";
-
-            if (!int.TryParse(list[0], out accId))
-            {
-                accId = 0;
-                accName = list[0].ToLowerInvariant();
-            }
-
-            using (var accService = new AccountService())
-            {
-                var account = accId > 0 ? accService.GetById(accId) : accService.GetByName(CurrentUser.Id, accName);
-                if (account == null)
-                {
-                    Console.WriteLine("Can't find such account");
-                    return;
-                }
-                //todo requires refactoring
-                var exchange = ExchangeInfo.Exchanges[account.Type];
-                switch (account.Type)
-                {
-                    case AccountType.Yobit:
-                        {
-                            using (var api = exchange.Api)
-                            {
-                                var yobitApi = api as YobitApi;
-                                var result = yobitApi.GetActiveOrdersOfUser(pair, account);
-
-                                if (result == null || result.success != 1)
-                                    Console.WriteLine(string.Format("Something wrong: {0}", result.error));
-                                else
-                                {
-                                    var yobitSettings = account.YobitSettings;
-                                    if (yobitSettings.Counter == 0)
-                                        yobitSettings.Counter = 1;
-                                    else
-                                        yobitSettings.Counter++;
-
-                                    accService.UpdateSettings(account.Id, JsonHelper.ToJson(yobitSettings));
-
-                                    Console.WriteLine("Done: " + JsonHelper.ToJson(result));
-                                }
-                            }
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Is not supported yet.");
-                        break;
-                }
-            }
-
-            return;
-        }
-
-        private static void GetPairInfo(params string[] list)
-		{
-			if (list.Length != 2)
-			{
-				Console.WriteLine("You must enter Exchange type and Ticker code");
-				return;
-			}
-			var tickerCode = list[1];
-			var param = list[0];
-			int type;
-			if (!int.TryParse(param, out type))
-			{
-				Console.WriteLine(string.Format("You must enter valid Exchange type: {0}", ExchangeInfo.GetAccountTypes));
-				return;
-			}
-
-			using (var pairService = new PairService())
-			{
-				var info = pairService.GetPair((AccountType)type, tickerCode);
-				if (info == null)
-					Console.WriteLine("Nothing found");
-				else
-					Console.WriteLine(string.Format("Ticker info: {0}", JsonHelper.ToJson(info)));
-			}
-		}
-
-		private static void GetPairs(params string[] list)
-		{
-			if (list.Length != 1)
-			{
-				Console.WriteLine("You must enter only Exchange type");
-				return;
-			}
-
-			var param = list.FirstOrDefault();
-			int type = 0;
-			if (!int.TryParse(param, out type))
-			{
-				Console.WriteLine(string.Format("You must enter valid Exchange type: {0}", ExchangeInfo.GetAccountTypes));
-				return;
-			}
-
-			var eType = (AccountType)type;
-			if (!ExchangeInfo.Exchanges.ContainsKey(eType))
-			{
-				Console.WriteLine("It's not implemented Exchange type");
-				return;
-			}
-
-			var exchange = ExchangeInfo.Exchanges[eType];
-			using (ExchangeApi api = exchange.Api)
-			{
-				try
-				{
-					dynamic result = null;
-					using (var pairService = new PairService())
-					{
-						result = pairService.PullPairs(api);
-					}
-					if (result != null && result.IsSuccess)
-						Console.WriteLine("Pairs info received, you can use /tickerInfo [tickerCode] to get appropriate info");
-					else
-						Console.WriteLine(string.Format("Error when pull pairs: {0}", result));
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine("Unexpected error");
-				}
-			}
-		}
+      
 	}
 }
