8be3f4c00c0a5bc97359531c7f751a7e470e5e51
Romaha <romanko754@gmail.com>
2018-02-15 01:37:50 +0200

    Added ClientFactory & some abstraction

---
+++ TradingBot.Core/ClientAdapter.cs
@@ -0,0 +1,21 @@
+﻿
+namespace TradingBot.Core
+{
+	using Enums;
+
+	public class ClientAdapter : IClientAdapter
+	{
+		private IClientFactory _factory;
+		private IExchangeClient _client;
+
+		public ClientAdapter(IClientFactory factory)
+		{
+			_factory = factory;
+		}
+
+		public void SwitchAccount(AccountType type)
+		{
+			_client = _factory.Create(type);
+		}
+	}
+}
---
+++ TradingBot.Core/IClientAdapter.cs
@@ -0,0 +1,8 @@
+﻿
+namespace TradingBot.Core
+{
+	public interface IClientAdapter
+	{
+
+	}
+}
