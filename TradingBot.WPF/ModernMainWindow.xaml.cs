a04c737a0e0ba1df74cc4251a3f2563d7659cb20
Roman Aseiev <v-roasei@microsoft.com>
2018-03-08 15:34:22 +0200

    Currency control design

---
+++ TradingBot.WPF/ModernMainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+namespace TradingBot.WPF
+{
+	using System.Windows;
+	using Windows.Controls;
+
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class ModernMainWindow
+	{
+		public ModernMainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+
+		}
+	}
+}
