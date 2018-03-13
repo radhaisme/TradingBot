a804750403b54d410d46acca08ea5f0565f2a7a1
Roman Aseiev <v-roasei@microsoft.com>
2018-03-13 19:10:09 +0200

    Added numeric control

--- TradingBot.WPF/MainWindow.xaml.cs
+++ TradingBot.WPF/MainWindow.xaml.cs
@@ -1,14 +1,14 @@
 ﻿
 namespace TradingBot.WPF
 {
+	using Core.Entities;
+	using Models;
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Windows;
 	using System.Windows.Controls;
 	using System.Windows.Input;
-	using Core.Entities;
-	using Models;
 	using Yobit.Api;
 
 	/// <summary>
@@ -16,28 +16,32 @@ namespace TradingBot.WPF
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private readonly YobitClient _client;
+		private Pair _pair;
+
 		public MainWindow()
 		{
+			_client = new YobitClient("https://yobit.net/api/3/", "https://yobit.net/tapi/", new YobitSettings
+			{
+				Secret = "5ceeeb6072789d30e79a961335e63d50",
+				ApiKey = "B03E731C650825B49CB2840E8449D98D",
+				CreatedAt = new DateTime(2018, 1, 1)
+			});
 			InitializeComponent();
 			Load();
 		}
 
 		private void DataGridMouseDoubleClick(object sender, MouseButtonEventArgs e)
 		{
-			var pair = (Pair)((DataGridRow)sender).Item;
-			Price.Text = pair.Fee.ToString();
-			Title = String.Format("{0}:{1}", pair.Name, pair.Fee);
+			_pair = (Pair)((DataGridRow)sender).Item;
+			Amount.Increment = Price.Increment = (double)new decimal(1, 0, 0, false, _pair.DecimalPlaces);
+			Price.Value = (double)_pair.MaxPrice;
+			//Title = String.Format("{0}:{1}", pair.Name, pair.Fee);
 		}
 
 		private async void Load()
 		{
-			var client = new YobitClient("https://yobit.net/api/3/", "https://yobit.net/tapi/", new YobitSettings
-			{
-				Secret = "5ceeeb6072789d30e79a961335e63d50",
-				ApiKey = "B03E731C650825B49CB2840E8449D98D",
-				CreatedAt = new DateTime(2018, 1, 1)
-			});
-			var pairs = await client.GetPairsAsync();
+			var pairs = await _client.GetPairsAsync();
 			IDictionary<string, List<Pair>> result = pairs.Pairs
 				.Where(x => !x.Value.IsHidden)
 				.Select(x =>
@@ -54,14 +58,41 @@ namespace TradingBot.WPF
 			DataContext = model;
 		}
 
-		private void BuyClick(object sender, RoutedEventArgs e)
+		private async void BuyClick(object sender, RoutedEventArgs e)
 		{
-			
+			if (_pair == null)
+			{
+				return;
+			}
+
+			CreateOrder order = await _client.CreateOrderAsync("", OrderType.Buy, 0, 0);
 		}
 
-		private void SellClick(object sender, RoutedEventArgs e)
+		private async void SellClick(object sender, RoutedEventArgs e)
 		{
+			if (_pair == null)
+			{
+				return;
+			}
 
+			CreateOrder order = await _client.CreateOrderAsync("", OrderType.Sell, 0, 0);
+		}
+
+		private bool ValidateOrderParams()
+		{
+			if (_pair == null)
+			{
+				return false;
+			}
+
+			if (Price.Value == 0)
+			{
+				return false;
+			}
+
+
+
+			return true;
 		}
 	}
 }
