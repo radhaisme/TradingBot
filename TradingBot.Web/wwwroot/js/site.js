9aa95ab23dbc26bd348356c188fdc99f80a7f13d
Romaha <romanko754@gmail.com>
2018-06-11 23:22:27 +0300

    Add ticker

--- TradingBot.Web/wwwroot/js/site.js
+++ TradingBot.Web/wwwroot/js/site.js
@@ -1,16 +1,27 @@
-﻿fetch("/home/getpairs", { method: "GET" })
-	.then((response) => {
-		return response.json();
-	})
-	.then((json) => {
-		console.log(json);
-	});
-setInterval(() => {
-	fetch("/home/getpairdata" + "?pair=ltc_btc", { method: "GET" })
-		.then((response) => {
-			return response.json();
-		})
-		.then((json) => {
-			console.log(json);
-		});
-}, 3000);
+var Ticker = /** @class */ (function () {
+    function Ticker(pair, interval) {
+        if (interval === void 0) { interval = 3000; }
+        var _this = this;
+        this._pair = pair;
+        this._interval = interval;
+        //fetch("/home/getpairs", { method: "GET" })
+        //	.then((response) => {
+        //		return response.json();
+        //	})
+        //	.then((json) => {
+        //		console.log(json);
+        //	});
+        setInterval(function () {
+            fetch("/home/getpairdata?pair=" + _this._pair, { method: "GET" })
+                .then(function (response) {
+                return response.json();
+            })
+                .then(function (json) {
+                console.log(json);
+            });
+        }, this._interval);
+    }
+    return Ticker;
+}());
+var ticker = new Ticker("ltc_btc");
+//# sourceMappingURL=site.js.map
