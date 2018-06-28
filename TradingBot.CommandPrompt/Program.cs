d446e5a483d2cf49fd0e7c272c84d18b738ba8c1
Roman <romanko754@gmail.com>
2018-06-29 00:41:57 +0300

    Add ExchangePair model

--- TradingBot.CommandPrompt/Program.cs
+++ TradingBot.CommandPrompt/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using TradingBot.Core;
+using TradingBot.Core.Entities;
 
 namespace TradingBot.CommandPrompt
 {
@@ -7,9 +10,35 @@ namespace TradingBot.CommandPrompt
 	{
 		private static async Task Main(string[] args)
 		{
-			
+
 
 			Console.ReadLine();
+		}
+	}
+
+	public class ExchangePair
+	{
+		private readonly IExchangeClient _first;
+		private readonly IExchangeClient _second;
+		private readonly IEnumerable<Pair> _pairs;
+
+		public ExchangePair(IExchangeClient first, IExchangeClient second)
+		{
+			_first = first;
+			_second = second;
+			_pairs = new List<Pair>();
+		}
+
+		public void Transfer(string symbol, bool swap)
+		{
+			if (swap)
+			{
+				//TODO: Swap exchanges
+
+				return;
+			}
+
+			//TODO: Transfer coins from the firs to the second exchange
 		}
 	}
 }
